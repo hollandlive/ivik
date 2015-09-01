78e9bce23b94a082cee3998f8b9c6b9f78350e3e
Artem <dutchnewsru@gmail.com>
2015-09-01 14:58:50 +0200

    createDeck.php
    Standing alone function createDeck.php (just experimenting)

---
+++ blackjack/includes/functions_createDeck.php
@@ -0,0 +1,24 @@
+<?php
+function createDeck (){
+$colors = array (
+    "Spades", "Hearts", "Clubs", "Diamonds"
+);
+
+$cards = array (
+    "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+    "Nine", "Ten", "Jack", "Queen", "King", "Ace"
+);
+
+//function createDeck - it will create a deck of 52 cards. Todo - wrap it into a function
+$deck = array();
+
+foreach ($colors as $color) {
+    foreach ($cards as $card) {
+        $deck[] = array ("card"=>$card, "color"=>$color);
+    }
+}
+return ($deck);
+}
+print_r (createDeck());
+
+?>
