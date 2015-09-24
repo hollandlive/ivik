373546efb501ee3ad133340d50da4eec45a9b8df
Artem <dutchnewsru@gmail.com>
2015-09-24 21:57:49 +0200

    Update f01.php

--- f01.php
+++ f01.php
@@ -6,6 +6,13 @@
 $a = 0;
 //if (--$a == 0) echo '-1 is not == 0' . $a; //will echo nothing and programm stops
 if ($a-- == 0) echo 'True, 0 == 0 and after checking $a is decremented and  becomes ' . $a .'<br>'; //will echo 'True, 0==0 and after checking $a is decremented and  becomes --1'
-
-echo 'Hello';
+/* Now example
+* with created
+* function*/
+function abc() {
+	$a = 0;
+	if ($a-- == 0) $b = $a;//'wwwwTrue, 0 == 0 and after checking $a is decremented and  becomes ' . $a .' '; //will echo 'True, 0==0 and after checking $a is decremented and  becomes --1'
+	return $b;
+}
+echo '<br>' . ' Now ' . abc();
 ?>
