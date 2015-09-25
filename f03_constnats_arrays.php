79629cd3c9d055eaf80938cc952aa0594752fbdd
Artem <dutchnewsru@gmail.com>
2015-09-25 21:47:35 +0200

    Create f03_constnats_arrays.php

---
+++ f03_constnats_arrays.php
@@ -0,0 +1,10 @@
+<?php
+/* This is an example,
+* and I am trying arrays 
+and constants
+in my simple program*/
+$a = array(	'1'=>1,
+			'2'=>2);
+echo $a[1];
+echo 'This is line' . __LINE__ . 'of a program';
+?>
